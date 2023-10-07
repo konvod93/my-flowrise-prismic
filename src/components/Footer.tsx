18ca23b090d8373f93db46b7b0ddb98709d928c8
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-10-07 15:43:23 +0300

    feat: Add SVG Logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Logo from "@/components/Logo";
+import Bounded from "@/components/Bounded";
 
 
 export default async function Footer() {
@@ -8,15 +10,23 @@ export default async function Footer() {
 
     const settings = await client.getSingle("settings");
 
-    return <footer>
-        <Link href="/">{settings.data.site_title}</Link>
-        <p>&copy;{new Date().getFullYear()} {settings.data.site_title}</p>
-        <ul>
-        {settings.data.navigation.map(({link, label}) => (
-            <li key={label}>
-                <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-        ))}
-    </ul>
-    </footer>
+    return (
+      <Bounded as="footer">
+        <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+          <Link href="/">
+            <Logo />
+          </Link>
+          <p className="text-sm">
+            &copy;{new Date().getFullYear()} {settings.data.site_title}
+          </p>
+          <ul className="flex">
+            {settings.data.navigation.map(({ link, label }) => (
+              <li key={label}>
+                <PrismicNextLink field={link} className="p-3">{label}</PrismicNextLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Bounded>
+    );
 }
